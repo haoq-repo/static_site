78c9d6e78ad751302edefac1bfe0b32276fdf56e
haoq_repo <qin.hao9@gmail.com>
2025-03-28 16:49:58 -0400

    feature: added leafnode and unit tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -28,3 +28,19 @@ class HTMLNode:
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+    
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, attributes=None):
+        super().__init__(tag, value, None, attributes)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("All leaf nodes *must* have a value")
+        if self.tag is None:
+            return self.value
+        
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"    
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
