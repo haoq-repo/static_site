b9338e0b97ca10b5c28b349db6d41732ce4572ca
haoq_repo <qin.hao9@gmail.com>
2025-03-30 13:24:13 -0400

    feature: add conversion of text to html

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import *
 
 
 class TestTextNode(unittest.TestCase):
@@ -29,6 +29,41 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(
             "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
         )
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is a bold node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.to_html(), "<b>This is a bold node</b>")
+
+    def test_italic(self):
+        node = TextNode("This is an italic node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.to_html(), "<i>This is an italic node</i>")
+
+    def test_code(self):
+        node = TextNode("This is a code node", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.to_html(), "<code>This is a code node</code>")
+
+    def test_link(self):
+        node = TextNode("This is a link node", TextType.LINK, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.to_html(), "<a href=\"https://www.boot.dev\">This is a link node</a>")
+
+    def test_image(self):
+        node = TextNode("This is an image node", TextType.IMAGE, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.to_html(), "<img src=\"https://www.boot.dev\" alt=\"This is an image node\"></img>")
+   
+    def test_fail(self):
+        with self.assertRaises(AttributeError):
+            node = TextNode("This is a link node", TextType.FAIL)
 
 if __name__ == "__main__":
     unittest.main()
